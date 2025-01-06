a79b39b96097bab3f0f2774ad378ed2b3637cd7b
Rajat Chandra <rajatc19@gmail.com>
2025-01-06 14:51:40 +0530

    feat: Enhance Hero component with automatic slide transition and improve carousel functionality

--- frontend/india-ecom/src/pages/Home/components/Hero.jsx
+++ frontend/india-ecom/src/pages/Home/components/Hero.jsx
@@ -2,128 +2,143 @@ import { useMemo, useEffect, useRef, useState } from "react";
 import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
 
 const images = import.meta.glob(
-    "../../../assets/hero/*.{jpg,jpeg,png,webp,avif}",
-    { eager: true }
+  "../../../assets/hero/*.{jpg,jpeg,png,webp,avif}",
+  { eager: true }
 );
 
 const HeroImage = ({ image, ref }) => {
-    return (
-        <div
-            className="grid grid-cols-4 w-screen shrink-0 items-center justify-center"
-            ref={ref}
-        >
-            <img
-                className="w-full object-cover col-span-1 blur-lg"
-                src={image.default}
-                alt=""
-            />
+  return (
+    <div
+      className="grid grid-cols-4 w-screen shrink-0 items-center justify-center"
+      ref={ref}
+    >
+      <img
+        className="w-full object-cover col-span-1 blur-lg"
+        src={image.default}
+        alt=""
+      />
 
-            <img
-                className="w-full object-cover max-h-[600px] col-span-2"
-                src={image.default}
-                alt=""
-            />
+      <img
+        className="w-full object-cover max-h-[600px] col-span-2"
+        src={image.default}
+        alt=""
+      />
 
-            <img
-                className="w-full object-cover col-span-1 blur-lg"
-                src={image.default}
-                alt=""
-            />
-        </div>
-    );
+      <img
+        className="w-full object-cover col-span-1 blur-lg"
+        src={image.default}
+        alt=""
+      />
+    </div>
+  );
 };
 
 const HeroPageButton = () => {
-    return <button className="bg"></button>;
+  return <button className="bg"></button>;
 };
 
 const Hero = () => {
-    const imageArray = useMemo(() => Object.values(images), []);
+  const imageArray = useMemo(() => Object.values(images), []);
 
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const heroSectionRef = useRef(null);
-    const heroCarouselRefs = useRef([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const heroCarouselRefs = useRef([]);
 
-    useEffect(() => {
-        const heroCarouselResize = () => {
-            heroCarouselRefs.current[currentSlide].scrollIntoView({
-                behavior: "instant",
-            });
-        };
-
-        window.addEventListener("resize", heroCarouselResize);
-
-        return () => window.removeEventListener("resize", heroCarouselResize);
-    }, [currentSlide, heroCarouselRefs]);
-
-    const scrollNext = () => {
-        const next = currentSlide + 1;
-        if (next < heroCarouselRefs.current.length) {
-            heroCarouselRefs.current[next].scrollIntoView({ behavior: "smooth" });
-            setCurrentSlide(next);
-        }
+  useEffect(() => {
+    const heroCarouselResize = () => {
+      heroCarouselRefs.current[currentSlide].scrollIntoView({
+        behavior: "instant",
+      });
     };
 
-    const scrollPrev = () => {
-        const prev = currentSlide - 1;
-        if (prev >= 0) {
-            heroCarouselRefs.current[prev].scrollIntoView({ behavior: "smooth" });
-            setCurrentSlide(prev);
-        }
-    };
+    window.addEventListener("resize", heroCarouselResize);
 
-    return (
-        <section className="relative h-fit">
-            <div className="absolute left-0 top-1/2 z-10">
-                <button
-                    className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
-                    onClick={() => scrollPrev()}
-                >
-                    <SlArrowLeft size={30} />
-                </button>
-            </div>
-            <div
-                className="overflow-hidden flex flex-nowrap gap-0.5 h-full"
-                ref={heroSectionRef}
-            >
-                {imageArray &&
-                    imageArray.map((image, i) => (
-                        <HeroImage
-                            image={image}
-                            key={i}
-                            ref={(e) => (heroCarouselRefs.current[i] = e)}
-                        />
-                    ))}
-            </div>
-            <div className="absolute right-0 top-1/2 z-10">
-                <button
-                    className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
-                    onClick={() => scrollNext()}
-                >
-                    <SlArrowRight size={30} />
-                </button>
-            </div>
-            <div className="absolute bottom-2 w-screen">
-            <div className="flex justify-center">
-                {imageArray &&
-                    imageArray.map((image, i) => (
-                        <button
-                            className={"p-1.5 m-1 cursor-pointer rounded-4xl " + ((currentSlide == i) ? "bg-amber-600" : "bg-white hover:bg-amber-600/50")}
-                            key={i}
-                            onClick={() => {
-                                heroCarouselRefs.current[i].scrollIntoView({
-                                    behavior: "smooth",
-                                });
-                                setCurrentSlide(i);
-                            }}
-                        >
-                            {" "}
-                        </button>
-                    ))}
-            </div>
-            </div>
-        </section>
-    );
+    return () => window.removeEventListener("resize", heroCarouselResize);
+  }, [currentSlide, heroCarouselRefs]);
+
+  const scrollNext = () => {
+    const next = currentSlide + 1;
+    if (next < heroCarouselRefs.current.length) {
+      heroCarouselRefs.current[next].scrollIntoView({ behavior: "smooth" });
+      setCurrentSlide(next);
+    }
+  };
+
+  const scrollPrev = () => {
+    const prev = currentSlide - 1;
+    if (prev >= 0) {
+      heroCarouselRefs.current[prev].scrollIntoView({ behavior: "smooth" });
+      setCurrentSlide(prev);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentSlide === heroCarouselRefs.current.length - 1) {
+        console.log("here");
+        heroCarouselRefs.current[0].scrollIntoView({ behavior: "instant" });
+        setCurrentSlide(0);
+      } else {
+        scrollNext();
+      }
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [currentSlide]);
+
+  return (
+    <section className="relative h-fit">
+      <div className="absolute left-0 top-1/2 z-10">
+        <button
+          className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
+          onClick={() => scrollPrev()}
+        >
+          <SlArrowLeft size={30} />
+        </button>
+      </div>
+      <div className="overflow-hidden flex flex-nowrap gap-0.5 h-full">
+        {imageArray &&
+          imageArray.map((image, i) => (
+            <HeroImage
+              image={image}
+              key={i}
+              ref={(e) => (heroCarouselRefs.current[i] = e)}
+            />
+          ))}
+      </div>
+      <div className="absolute right-0 top-1/2 z-10">
+        <button
+          className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
+          onClick={() => scrollNext()}
+        >
+          <SlArrowRight size={30} />
+        </button>
+      </div>
+      <div className="absolute bottom-2 w-screen">
+        <div className="flex justify-center">
+          {imageArray &&
+            imageArray.map((image, i) => (
+              <button
+                className={
+                  "p-1.5 m-1 cursor-pointer rounded-4xl " +
+                  (currentSlide == i
+                    ? "bg-amber-600"
+                    : "bg-white hover:bg-amber-600/50")
+                }
+                key={i}
+                onClick={() => {
+                  heroCarouselRefs.current[i].scrollIntoView({
+                    behavior: "smooth",
+                  });
+                  setCurrentSlide(i);
+                }}
+              >
+                {" "}
+              </button>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
