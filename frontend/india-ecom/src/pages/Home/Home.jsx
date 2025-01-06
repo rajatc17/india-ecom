fa9f9782ebba5ea54781ca30907daba056ac1b93
Rajat Chandra <rajatc19@gmail.com>
2025-01-06 15:10:47 +0530

    feat: Implement category management with fetchCategoryTree and integrate into SubHeader component

--- frontend/india-ecom/src/pages/Home/Home.jsx
+++ frontend/india-ecom/src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import Hero from "./components/Hero";
 import SubHeader from "./components/SubHeader";
 import { useSelector , useDispatch } from "react-redux";
 import { fetchProducts } from "../../store/product/productSlice";
+import { fetchCategoryTree } from "../../store/category/categorySlice";
 
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.items);
   
   useEffect(()=>{
-    dispatch(fetchProducts());
+    dispatch(fetchProducts({page: 1, limit : 100}));
+    dispatch(fetchCategoryTree());
   }, [])
 
   return (
--- frontend/india-ecom/src/pages/Home/components/SubHeader.jsx
+++ frontend/india-ecom/src/pages/Home/components/SubHeader.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+
+const SHItemMenu = () =>{
+  
+}
+
+const SHItem = ( {name} ) => {
+  return (
+    <div className=''>
+      {name}
+    </div>
+  )
+}
 
 const SubHeader = () => {
+  const categoryTree = useSelector((state) => state.categories.items)
+  
+  useEffect(()=>{
+    console.log(categoryTree)
+  },[categoryTree])
+
   return (
-    <div>SubHeader</div>
+    <div className='flex gap-1 justify-between px-4 py-6 text-md'>
+      {
+        categoryTree && categoryTree.map((category,i) => <SHItem name={category.name} key={category.id}/>)
+      }
+    </div>
   )
 }
 
