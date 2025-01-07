834a31f386501404c35635b60c7cbec19bee86e4
Rajat Chandra <rajatc19@gmail.com>
2025-01-07 16:36:33 +0530

    feat: Enhance Hero component with carousel functionality and navigation buttons

--- frontend/india-ecom/src/pages/Home/components/Hero.jsx
+++ frontend/india-ecom/src/pages/Home/components/Hero.jsx
@@ -1,42 +1,81 @@
-import React, { useMemo } from "react";
+import React, { useMemo,useEffect,useRef, useState, } from "react";
+import { SlArrowRight , SlArrowLeft } from "react-icons/sl";
 const images = import.meta.glob(
-  "../../../assets/hero/*.{jpg,jpeg,png,webp,avif}",
-  { eager: true }
+    "../../../assets/hero/*.{jpg,jpeg,png,webp,avif}",
+    { eager: true }
 );
 
-const HeroImage = ({ image }) => {
-  return (
-    <div className="grid grid-cols-4 w-lvw shrink-0 items-center justify-center">
-      <img
-        className="w-full object-cover col-span-1 blur-lg"
-        src={image.default}
-        alt=""
-      />
+const HeroImage = ({ image , ref }) => {
 
-      <img
-        className="w-full object-cover max-h-[500px] col-span-2"
-        src={image.default}
-        alt=""
-      />
+    return (
+        <div className="grid grid-cols-4 w-screen shrink-0 items-center justify-center" ref={ref}>
+            <img
+                className="w-full object-cover col-span-1 blur-lg"
+                src={image.default}
+                alt=""
+            />
 
-      <img
-        className="w-full object-cover col-span-1 blur-lg"
-        src={image.default}
-        alt=""
-      />
-    </div>
-  );
+            <img
+                className="w-full object-cover max-h-[500px] col-span-2"
+                src={image.default}
+                alt=""
+            />
+
+            <img
+                className="w-full object-cover col-span-1 blur-lg"
+                src={image.default}
+                alt=""
+            />
+        </div>
+    );
 };
 
 const Hero = () => {
-  const imageArray = useMemo(() => Object.values(images), []);
-  console.log(imageArray);
+    const imageArray = useMemo(() => Object.values(images), []);
 
-  return (
-    <section className="h-[500px] overflow-y-hidden flex flex-nowrap gap-0.5 ">
-      {imageArray && imageArray.map((image) => <HeroImage image={image} />)}
-    </section>
-  );
+    const [currentSlide , setCurrentSlide] = useState(0);
+
+    const heroCarouselRefs = useRef([]);
+
+    useEffect(()=>{
+        console.log(heroCarouselRefs)
+    },[])
+
+    const scrollNext = () => {
+        const next = currentSlide + 1;
+        console.log(next);
+        if( next < heroCarouselRefs.current.length ){
+            heroCarouselRefs.current[next].scrollIntoView({behavior: "smooth"})
+            setCurrentSlide(next);
+        }
+    }
+
+    const scrollPrev = () => {
+        const prev = currentSlide - 1;
+        console.log(prev);
+        if( prev >= 0 ){
+            heroCarouselRefs.current[prev].scrollIntoView({behavior: "smooth"})
+            setCurrentSlide(prev);
+        }
+    }
+
+    return (
+        <section className="relative h-[500px]">
+            <div className="absolute left-0 top-1/2 z-10">
+                <button className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer" onClick={() => scrollPrev()}>
+                    <SlArrowLeft size={30} />
+                </button>
+            </div>
+            <div className="overflow-hidden flex flex-nowrap gap-0.5 border-2 h-[500px]" >
+                {imageArray && imageArray.map((image , i) => <HeroImage image={image} key={i} ref={(e) => heroCarouselRefs.current[i] = e} />)}
+            </div>
+            <div className="absolute right-0 top-1/2 z-10">
+               <button className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer" onClick={() => scrollNext()}>
+                    <SlArrowRight size={30} />
+                </button>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
