da7c6d76e26c7562e8d53db1580d0294f7ce57de
Rajat Chandra <rajatc19@gmail.com>
2025-01-07 13:35:24 +0530

    refactor: Clean up Hero component code and adjust styles for better layout

--- frontend/india-ecom/src/pages/Home/components/Hero.jsx
+++ frontend/india-ecom/src/pages/Home/components/Hero.jsx
@@ -1,14 +1,17 @@
-import React, { useMemo,useEffect,useRef, useState, } from "react";
-import { SlArrowRight , SlArrowLeft } from "react-icons/sl";
+import { useMemo, useEffect, useRef, useState } from "react";
+import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
+
 const images = import.meta.glob(
     "../../../assets/hero/*.{jpg,jpeg,png,webp,avif}",
     { eager: true }
 );
 
-const HeroImage = ({ image , ref }) => {
-
+const HeroImage = ({ image, ref }) => {
     return (
-        <div className="grid grid-cols-4 w-screen shrink-0 items-center justify-center" ref={ref}>
+        <div
+            className="grid grid-cols-4 w-screen shrink-0 items-center justify-center"
+            ref={ref}
+        >
             <img
                 className="w-full object-cover col-span-1 blur-lg"
                 src={image.default}
@@ -16,7 +19,7 @@ const HeroImage = ({ image , ref }) => {
             />
 
             <img
-                className="w-full object-cover max-h-[500px] col-span-2"
+                className="w-full object-cover max-h-[600px] col-span-2"
                 src={image.default}
                 alt=""
             />
@@ -33,44 +36,64 @@ const HeroImage = ({ image , ref }) => {
 const Hero = () => {
     const imageArray = useMemo(() => Object.values(images), []);
 
-    const [currentSlide , setCurrentSlide] = useState(0);
-
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const heroSectionRef = useRef(null)
     const heroCarouselRefs = useRef([]);
 
-    useEffect(()=>{
-        console.log(heroCarouselRefs)
-    },[])
+    useEffect(() => {
+        const heroCarouselResize = () => {
+            heroCarouselRefs.current[currentSlide].scrollIntoView({
+                behavior: 'instant'
+            })
+        }
+
+        window.addEventListener('resize', heroCarouselResize)
+
+        return () => window.removeEventListener('resize', heroCarouselResize)
+
+    }, [currentSlide, heroCarouselRefs])
 
     const scrollNext = () => {
         const next = currentSlide + 1;
-        console.log(next);
-        if( next < heroCarouselRefs.current.length ){
-            heroCarouselRefs.current[next].scrollIntoView({behavior: "smooth"})
+        if (next < heroCarouselRefs.current.length) {
+            heroCarouselRefs.current[next].scrollIntoView({ behavior: "smooth" });
             setCurrentSlide(next);
         }
-    }
+    };
 
     const scrollPrev = () => {
         const prev = currentSlide - 1;
-        console.log(prev);
-        if( prev >= 0 ){
-            heroCarouselRefs.current[prev].scrollIntoView({behavior: "smooth"})
+        if (prev >= 0) {
+            heroCarouselRefs.current[prev].scrollIntoView({ behavior: "smooth" });
             setCurrentSlide(prev);
         }
-    }
+    };
 
     return (
-        <section className="relative h-[500px]">
+        <section className="relative h-[600px]">
             <div className="absolute left-0 top-1/2 z-10">
-                <button className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer" onClick={() => scrollPrev()}>
+                <button
+                    className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
+                    onClick={() => scrollPrev()}
+                >
                     <SlArrowLeft size={30} />
                 </button>
             </div>
-            <div className="overflow-hidden flex flex-nowrap gap-0.5 border-2 h-[500px]" >
-                {imageArray && imageArray.map((image , i) => <HeroImage image={image} key={i} ref={(e) => heroCarouselRefs.current[i] = e} />)}
+            <div className="overflow-hidden flex flex-nowrap gap-0.5 h-full" ref={heroSectionRef}>
+                {imageArray &&
+                    imageArray.map((image, i) => (
+                        <HeroImage
+                            image={image}
+                            key={i}
+                            ref={(e) => (heroCarouselRefs.current[i] = e)}
+                        />
+                    ))}
             </div>
             <div className="absolute right-0 top-1/2 z-10">
-               <button className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer" onClick={() => scrollNext()}>
+                <button
+                    className="bg-black/55 p-2 rounded-4xl text-white cursor-pointer"
+                    onClick={() => scrollNext()}
+                >
                     <SlArrowRight size={30} />
                 </button>
             </div>
