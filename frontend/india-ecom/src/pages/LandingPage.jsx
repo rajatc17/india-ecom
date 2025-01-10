38da91178417c9e351e7b2a039953654c3ce9aae
Rajat Chandra <rajatc19@gmail.com>
2025-01-10 13:26:44 +0530

    Refactor Header and Footer components to use semantic HTML elements; update LandingPage component structure

--- frontend/india-ecom/src/pages/LandingPage.jsx
+++ frontend/india-ecom/src/pages/LandingPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 const LandingPage = () => {
+  useEffect(() => {
+
+    const fetchProducts = async () => {
+        const data = await fetch('http://localhost:5000/api/products');
+        const json = await data.json();
+        console.log(json);
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>LandingPage</div>
+    <div>
+
+    </div>
   )
 }
 
