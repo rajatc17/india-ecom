5fefa195f3807f903d17c4f4aa6e50a96223a088
Rajat Chandra <rajatc19@gmail.com>
2025-10-13 14:25:50 +0530

    Wiring server with Atlas DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require('./db')
 
 const app = express();
 app.use(express.json());
@@ -7,9 +8,10 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.json({
     ok: true,
-    message: "Server health check!",
   });
 });
 
-
-app.listen(5000, ()=>console.log('Server is running on port 5000'));
+const PORT  = process.env.PORT || 5000;
+connectDB().then(()=>{
+    app.listen(PORT, ()=> console.log(`Server & DB running on ${PORT}`));
+});
