8f48d6f16723bc45133abbbd6fd2ffa71ce7c77c
Rajat Chandra <rajatc19@gmail.com>
2025-10-13 14:36:46 +0530

    Init Product model

---
+++ backend/src/models/Product.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: String, index: true },
+    subcategory: { type: String, index: true },
+    subsubcategory: { type: String, index: true }, // optional third level
+    price: { type: Number, required: true },
+    discountedPrice: Number,
+    stock: { type: Number, default: 0 },
+    material: String,
+    region: { type: String, index: true },
+    giTagged: { type: Boolean, default: false },
+    description: String,
+    images: [String],
+  },
+  { timestamps: true }
+);
