4ce141a814c7022ef39f195ba14636f04fb212e8
Rajat Chandra <rajatc19@gmail.com>
2025-10-17 23:48:41 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/india-ecom/src/components/Header.jsx
+++ frontend/india-ecom/src/components/Header.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import logo from '../assets/logoTP.png'
+import logoSVG from '../assets/logoSVG.svg'
+import { useState } from 'react';
 
 const Header = () => {
+  const [searchText, setSearchText] = useState()
   return (
-    <header className='w-full h-4 shadow-amber-200 bg-amber-100'>
-      <h1>HELLO</h1>
+    <header className="relative bg-terracotta px-8 py-12 overflow-hidden">
+      {/* Texture overlay - now using custom utility */}
+      <div className="absolute inset-0 bg-terracotta-texture opacity-30 pointer-events-none" />
+      
+      {/* Header content */}
+      <div className="relative z-10 max-w-8xl mx-auto">
+        <nav className="flex items-center justify-between px-10">
+          <div className="">
+            <img className='w-[150px] h-auto object-fill' src={logo} alt='Shilpika'/>
+          </div>
+          <div>
+            <input type='text' />
+          </div>
+          <ul className="flex gap-8 text-gold-light">
+            <li className="hover:text-gold transition cursor-pointer">Home</li>
+            <li className="hover:text-gold transition cursor-pointer">Crafts</li>
+            <li className="hover:text-gold transition cursor-pointer">Artisans</li>
+            <li className="hover:text-gold transition cursor-pointer">About</li>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
