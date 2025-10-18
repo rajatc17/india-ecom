54fdc1a9a18a3a4498747faaff959fb40800e968
Rajat Chandra <rajatc19@gmail.com>
2025-10-18 21:33:55 +0530

    Implement code changes to enhance functionality and improve performance

--- frontend/india-ecom/src/components/Header.jsx
+++ frontend/india-ecom/src/components/Header.jsx
@@ -1,28 +1,27 @@
-import logo from '../assets/logoTP.png'
-import logoSVG from '../assets/logoSVG.svg'
+import logoSVG from '../assets/logoSVG.svg';
+import logoNew from '../assets/logoNew.png';
 import { useState } from 'react';
 
 const Header = () => {
   const [searchText, setSearchText] = useState()
   return (
-    <header className="relative bg-terracotta px-8 py-12 overflow-hidden">
+    <header className="relative bg-white px-2 py-4 overflow-hidden shadow-xl">
       {/* Texture overlay - now using custom utility */}
       <div className="absolute inset-0 bg-terracotta-texture opacity-30 pointer-events-none" />
       
       {/* Header content */}
       <div className="relative z-10 max-w-8xl mx-auto">
         <nav className="flex items-center justify-between px-10">
-          <div className="">
-            <img className='w-[150px] h-auto object-fill' src={logo} alt='Shilpika'/>
-          </div>
           <div>
-            <input type='text' />
+            <input className=' rounded-2xl px-2' type='text' placeholder='Search' value={searchText}/>
           </div>
-          <ul className="flex gap-8 text-gold-light">
-            <li className="hover:text-gold transition cursor-pointer">Home</li>
-            <li className="hover:text-gold transition cursor-pointer">Crafts</li>
-            <li className="hover:text-gold transition cursor-pointer">Artisans</li>
-            <li className="hover:text-gold transition cursor-pointer">About</li>
+          <div className="">
+            <img className='w-auto h-[80px] object-cover mx-auto' src={logoNew} alt='Shilpika'/>
+          </div>
+          <ul className="flex gap-8 text-black font-semibold">
+            <li className="hover:text-gold transition cursor-pointer">LOGIN</li>
+            <li className="hover:text-gold transition cursor-pointer">WISHLIST</li>
+            <li className="hover:text-gold transition cursor-pointer">BAG</li>
           </ul>
         </nav>
       </div>
--- frontend/india-ecom/src/pages/LandingPage.jsx
+++ frontend/india-ecom/src/pages/LandingPage.jsx
@@ -1,22 +1,20 @@
-import {useEffect} from 'react'
+import { useEffect } from "react";
 
 const LandingPage = () => {
   useEffect(() => {
-
     const fetchProducts = async () => {
-        const data = await fetch('http://localhost:5000/api/products/featured');
-        const json = await data.json();
-        console.log(json);
-    }
+      const data = await fetch("http://localhost:5000/api/categories?level=0");
+      const json = await data.json();
+      console.log(json);
+    };
 
     fetchProducts();
   }, []);
 
   return (
-    <div>
-        
+    <div className="bg-amber-400">
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
