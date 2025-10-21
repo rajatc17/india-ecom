6b04a79bf89cb624284a1f96338d11ac61dcb727
Rajat Chandra <rajatc19@gmail.com>
2025-10-21 15:29:24 +0530

    feat: Add Home page and replace LandingPage component; include react-icons for search functionality

---
+++ frontend/india-ecom/src/pages/Home/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full'>
+        <picture>
+            
+        </picture>
+    </div>
+  )
+}
+
+export default Hero
